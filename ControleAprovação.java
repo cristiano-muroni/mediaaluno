b76d61885eabf220d880723871c5ede244ba9fbb
Cristiano Muroni <muronislick@hotmail.com>
2020-05-18 22:21:10 -0300

    interação para add varios alunos

--- ControleAprovação.java
+++ ControleAprovação.java
@@ -1,6 +1,8 @@
 package mediaaluno;
 // controle da classe Aluno e Disciplina
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -13,46 +15,57 @@ public class ControleAprovação {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
+		List<Aluno> alunos = new ArrayList<Aluno>();
 		Aluno aluno = new Aluno();
-        // capturando os dados do usuário
-		System.out.println(" Digite o Ra do aluno");
-		aluno.setRa(Integer.parseInt(sc.nextLine()));
-		System.out.println(" Digite o nome do aluno");
-		aluno.setNome(sc.nextLine());
-		System.out.println(" Digite o nome do pai do aluno");
-		aluno.setNomePai(sc.nextLine());
-		System.out.println(" Digite o nome do mãe do aluno");
-		aluno.setNomeMae(sc.nextLine());
-		System.out.println(" Digite o CPF aluno");
-		aluno.setNumeroCpf(Long.parseLong(sc.nextLine()));
-		System.out.println(" Digite a data de nascimento");
-		aluno.setDataNascimento(sc.nextLine());
-		System.out.println(" Digite o nome da Escola");
-		aluno.setNomeEscola(sc.nextLine());
-		String semestre = JOptionPane.showInputDialog(" digite o semestre do aluno");
-		aluno.setSemestre(semestre);
 
-		Disciplina disciplina = null;
+		for (int qtd = 1; qtd < 4; qtd++) {// inico do escopo para add varios alunos
 
-		for (int i = 1; i < 4; i++) {// escopo que scaneia para dentro do List, os dados vindo do usuário
+			// capturando os dados do usuário
+			System.out.println(" Digite o Ra do aluno");
+			aluno.setRa(Integer.parseInt(sc.nextLine()));
+			System.out.println(" Digite o nome do aluno " + qtd);
+			aluno.setNome(sc.nextLine());
+			System.out.println(" Digite o nome do pai do aluno");
+			aluno.setNomePai(sc.nextLine());
+			System.out.println(" Digite o nome do mãe do aluno");
+			aluno.setNomeMae(sc.nextLine());
+			System.out.println(" Digite o CPF aluno");
+			aluno.setNumeroCpf(Long.parseLong(sc.nextLine()));
+			System.out.println(" Digite a data de nascimento");
+			aluno.setDataNascimento(sc.nextLine());
+			System.out.println(" Digite o nome da Escola");
+			aluno.setNomeEscola(sc.nextLine());
+			String semestre = JOptionPane.showInputDialog(" digite o semestre do aluno");
+			aluno.setSemestre(semestre);
 
-			String nomeDisciplina = JOptionPane.showInputDialog(" Digite o nome da disciplina" + i + " ?");
-			String notaDisciplina = JOptionPane.showInputDialog(" Digite a nota da disciplina" + i + " ?");
-			disciplina = new Disciplina();
-			disciplina.setDisciplina(nomeDisciplina);
-			disciplina.setNota(Double.valueOf(notaDisciplina));
+			Disciplina disciplina = null;
 
-			aluno.getDisciplinas().add(disciplina);
+			for (int i = 1; i < 4; i++) {// escopo que scaneia para dentro do List, os dados vindo do usuário
 
-		}
+				String nomeDisciplina = JOptionPane.showInputDialog(" Digite o nome da disciplina" + i + " ?");
+				String notaDisciplina = JOptionPane.showInputDialog(" Digite a nota da disciplina" + i + " ?");
+				disciplina = new Disciplina();
+				disciplina.setDisciplina(nomeDisciplina);
+				disciplina.setNota(Double.valueOf(notaDisciplina));
 
-		for (Disciplina conteudo : aluno.getDisciplinas()) {//escopo que faz mostra na tela a lista de disciplinas e suas notas
+				aluno.getDisciplinas().add(disciplina);
+
+			}
+			for (Aluno alunoX : alunos) {
+				System.out.println(alunoX.toString());
+				System.out.printf("a sua media %.2f: \n", alunoX.getMediaNota());
+				System.out.println(" O aluno " + alunoX.getNome() + " está" + alunoX.getAprovacao());
+				System.out.println("-------------------------- * -------------------------------");
+
+			}
+
+			alunos.add(aluno);// add aluno na lista de alunos
+		} // fim do escopo para add varios alunos
+
+		for (Disciplina conteudo : aluno.getDisciplinas()) {// escopo que faz mostra na tela a lista de disciplinas e
+															// suas notas
 			System.out.println(conteudo.toString());
 		}
-
-		System.out.println(aluno.toString());
-		System.out.printf("a sua media %.2f: \n", aluno.getMediaNota());
-		System.out.println(" O aluno " + aluno.getNome() + " está" + aluno.getAprovacao());
 
 		int escolha = JOptionPane.showConfirmDialog(null, " Deseja remover alguma disciplina?");
 
