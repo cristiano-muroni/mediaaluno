cc216f552174e4ab31e03c24a1069364b2707dbf
Cristiano Muroni <muronislick@hotmail.com>
2020-05-18 21:22:58 -0300

    implementando remove interativo mult remove

--- ControleAprovação.java
+++ ControleAprovação.java
@@ -14,7 +14,7 @@ public class ControleAprovação {
 		Scanner sc = new Scanner(System.in);
 
 		Aluno aluno = new Aluno();
-
+        // capturando os dados do usuário
 		System.out.println(" Digite o Ra do aluno");
 		aluno.setRa(Integer.parseInt(sc.nextLine()));
 		System.out.println(" Digite o nome do aluno");
@@ -34,7 +34,7 @@ public class ControleAprovação {
 
 		Disciplina disciplina = null;
 
-		for (int i = 1; i < 4; i++) {
+		for (int i = 1; i < 4; i++) {// escopo que scaneia para dentro do List, os dados vindo do usuário
 
 			String nomeDisciplina = JOptionPane.showInputDialog(" Digite o nome da disciplina" + i + " ?");
 			String notaDisciplina = JOptionPane.showInputDialog(" Digite a nota da disciplina" + i + " ?");
@@ -46,7 +46,7 @@ public class ControleAprovação {
 
 		}
 
-		for (Disciplina conteudo : aluno.getDisciplinas()) {
+		for (Disciplina conteudo : aluno.getDisciplinas()) {//escopo que faz mostra na tela a lista de disciplinas e suas notas
 			System.out.println(conteudo.toString());
 		}
 
@@ -56,9 +56,20 @@ public class ControleAprovação {
 
 		int escolha = JOptionPane.showConfirmDialog(null, " Deseja remover alguma disciplina?");
 
-		if (escolha == 0) {
-			String disciplinaRemover = JOptionPane.showInputDialog("Qual será removida? \n a 1, 2, 3 ou 4 ?");
-			aluno.getDisciplinas().remove(Integer.valueOf(disciplinaRemover).intValue() - 1);
+		if (escolha == 0) {// trecho do código que faz a remoção de disciplinas
+
+			int continuaRemov = 0;
+			int position = 0;
+			while (continuaRemov == 0) {
+				String disciplinaRemover = JOptionPane.showInputDialog("Qual será removida? \n a 1, 2, 3 ou 4 ?");
+				aluno.getDisciplinas().remove(Integer.valueOf(disciplinaRemover).intValue() - position);
+				position++;
+				continuaRemov = JOptionPane.showConfirmDialog(null, "Continuar a remover ?");
+			} // final do trecho que faz a remoção de disciplinas
+		}
+		for (Disciplina conteudo : aluno.getDisciplinas()) {// escopo que mostra na tela todas a notas e discipinas que
+															// sobraram, não contendo mais os excluídos
+			System.out.println(conteudo.toString());
 		}
 
 		sc.close();
